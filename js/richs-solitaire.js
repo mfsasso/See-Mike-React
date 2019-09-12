a594e7d78263e098e375bef051239ae81db5903a
Mike Sasso <mike.f.sasso@gmail.com>
2019-09-12 15:03:03 -0400

    Add much-needed comments to a few files

--- js/richs-solitaire.js
+++ js/richs-solitaire.js
@@ -1,3 +1,7 @@
+/*
+ * JavaScript for SeeMikeReact.com
+ */
+
 var deck = [];
 var currentCard = 0;
 var loserFound = false;
@@ -6,11 +10,15 @@ var imageObj = [];
 const cardFaces = ['Ace','Two','Three','Four','Five','Six','Seven',
 	'Eight','Nine','Ten','Jack','Queen','King'];
 
+
+// A deck is an array of length 52 containing the values 0 to 51
 let generateDeck = () => {
 	let arr = [...Array(52)].map( (elementVal, idx) => idx);
 	return arr;
 }
 
+
+// Shuffle the deck: work through the deck randomly swapping pairs of cards
 let shuffleDeck = () => {
 	for (let i = 51; i >= 1; --i) {
 		let j = Math.floor(Math.random() * (i + 1));
@@ -22,22 +30,30 @@ let shuffleDeck = () => {
 	}
 }
 
+
+// DisplayCard is called to display a single card and determine if play should continue
 let displayCard = () => {
 
+	// Build a <div> containing an image of the card to display and the text of its value
 	let cardId = 'card' + currentCard;
 	let defn = '<div class="face-up" id="' + cardId + '"> <img';
 	if (currentCard % 13 == deck[currentCard] % 13) {
+		// Code runs if this card causes the game to be lost
 		defn += ' id="loser"';
 		loserFound = true;
 	}
 	defn += ' src="/images/card' + deck[currentCard] + '.png" />';
 	defn += '<p class="card-name">"' + cardFaces[currentCard % 13] + '"</p></div>';
+	
 	++currentCard;
+	
+	// Get the card image into the DOM and make sure it's visible
 	$( '#cards' ).append( defn );
 	$('#' + cardId)[0].scrollIntoView({
 		behavior: "smooth", block: "end"
 	});
 	
+	// If game has been won or lost, add node to DOM indicating the result
 	if (loserFound) {
 		$( '#cards' ).after( '<h2 id="game-result">Sorry, you lose</h2>' );
 		$( '#game-result' )[0].scrollIntoView({
@@ -51,6 +67,7 @@ let displayCard = () => {
 		});
 	}
 	
+	// If game was just won or lost, reset for another play
 	if (loserFound || currentCard >= 52) {
 		clearInterval( timerVar );
 		$( '#play-game' ).removeAttr( 'disabled' );
@@ -59,20 +76,31 @@ let displayCard = () => {
 	
 }
 
+
+// Kick off game play when the Play button is clicked
 let playIt = () => {
+
 	$( '#play-game' ).attr( 'disabled', true );
 	$( '#show-rules' ).attr( 'disabled', true );
+
 	deck = generateDeck();
+	
 	for( let i = 0; i < 100; ++i ) {
 		shuffleDeck();
 	}
+	
 	$( '#cards' ).empty();
 	$( '#game-result' ).remove();
+	
 	currentCard = 0;
 	loserFound = false;
+	
+	// Display a card every half second till the game is won or lost
 	timerVar = setInterval( displayCard, 500);
 }
 
+
+// Preload card images so game isn't slowed down the first time it's played after page load
 $( document ).ready( () => {
 	var imageSrc = ''
 	var i = 0;
--- reactserver.js
+++ reactserver.js
@@ -1,19 +1,24 @@
+/*
+ * Server code for SeeMikeReact.com
+ */
+
 var express = require( 'express' );
 var app = express();
 var router = express.Router();
 
+// HTTP Get request. Send HTML for the page.
 router.get( '/', (req, res ) => {
     res.sendFile( __dirname + '/html/index.html' );
 });
 
+// Handle requests for file assets
 app.use( '/', router );
 app.use(express.static('.'));
 
+// Start server
 app.listen( 80, () => {
     console.log( 'Server running on port 80' );
 });
 
-setInterval( () => {let x = Math.random()}, 500 );
-/* while( true ) {
-	let x = Math.random();
-} */
+// For future expansion
+// setInterval( () => {let x = Math.random()}, 500 );
